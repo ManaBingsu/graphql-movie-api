2360654e24c0daf27c98759d5a754dd091bb8216
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-22 14:38:30 +0900

    Wrapping a REST API part 1

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,8 @@
-let movies = [
-    {
-        id: 0,
-        name:"HyunSeok0",
-        score: 2
-    },
-    {
-        id: 1,
-        name:"HyunSeok1",
-        score: 5
-    },
-    {
-        id: 2,
-        name:"HyunSeok2",
-        score: 10
-    }
-];
+import fetch from "node-fetch";
+const API_URL = "htps://yts.am/api/v2/list_movies.json"
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    }
-    movies.push(newMovie);
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 }
